b6b333f64b96d31c44db6da69a08f500bffee75f
Laihonen Ruusu K <rula@cubbli24-gold.cs.helsinki.fi>
2025-04-04 14:16:23 +0300

    Show post list/info

--- app.py
+++ app.py
@@ -12,8 +12,13 @@ app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    all_posts = posts.get_posts()
+    return render_template("index.html", posts=all_posts)
 
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    post = posts.get_post(post_id)
+    return render_template("show_post.html", post=post)
 
 @app.route("/new_post")
 def new_post():
--- posts.py
+++ posts.py
@@ -4,3 +4,17 @@ def add_post(descriptio, user_id):
     sql = """INSERT INTO posts (descriptio, user_id)
             VALUES (?, ?)"""
     db.execute(sql, [descriptio, user_id])
+
+def get_posts():
+    sql = "SELECT id, descriptio FROM posts ORDER BY id DESC"
+    return db.query(sql)
+
+def get_post(post_id):
+    sql = """
+        SELECT posts.descriptio, users.username
+        FROM posts
+        JOIN users ON posts.user_id = users.id
+        WHERE posts.id = ?
+    """
+    results = db.query(sql, [post_id])
+    return results[0] if results else None
