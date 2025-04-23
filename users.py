dad7f9a974c09223f88544a15890289afaefbc41
Laihonen Ruusu K <rula@cubbli24-gold.cs.helsinki.fi>
2025-04-23 16:51:56 +0300

    Change layout

--- users.py
+++ users.py
@@ -7,7 +7,7 @@ def get_user(user_id):
     return results[0] if results else None
 
 def get_posts(user_id):
-    sql = "SELECT id, title FROM posts WHERE user_id = ? ORDER BY id DESC"
+    sql = "SELECT id, title, image FROM posts WHERE user_id = ? ORDER BY id DESC"
     return db.query(sql, [user_id])
 
 def create_user(username, password):
